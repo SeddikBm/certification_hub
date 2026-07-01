2d59a7e7e916bf20184262fa3b0c6dc5eed47386
SeddikBm <seddik_boumhamdi@um5.ac.ma>
2026-07-01 17:03:11 +0100

    Init backend structure, PostgreSQL connection, and Flyway

---
+++ backend-spring/src/main/java/com/example/certificationHub/CertificationHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.certificationHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificationHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificationHubApplication.class, args);
+	}
+
+}
---
+++ backend-spring/src/test/java/com/example/certificationHub/CertificationHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.certificationHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificationHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
